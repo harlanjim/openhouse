03bc6e39188674d7df6c8d7f0c7a0932b765c754
jim <fah.jim@gmail.com>
2016-06-02 16:59:44 -0500

    Initial

---
+++ openhouse/src/main/java/com/fah/openhouse/OpenhouseApplication.java
@@ -0,0 +1,13 @@
+package com.fah.openhouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenhouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenhouseApplication.class, args);
+	}
+}
